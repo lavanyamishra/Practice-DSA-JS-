8cf0b4abfa41eba1b9e41ac783359cf1d2ecd189
lavanyamishra <100487476+lavanyamishra@users.noreply.github.com>
2026-06-24 10:28:20 +0530

    Time: 1 ms (25.53%), Space: 54.4 MB (34.45%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -4,15 +4,13 @@
  */
 var balancedStringSplit = function (s) {
     let R = 0;
-    let L = 0;
     let count = 0;
+    let temp = 0;
     for (let i = 0; i < s.length; i++) {
-        if (s[i] === 'R') R++;
-        else L++;
-        if (R === L) {
+        if (s[i] === 'R') temp++;
+        else temp--;
+        if (temp === 0) {
             count++;
-            R = 0;
-            L = 0;
         }
     }
     return count;
