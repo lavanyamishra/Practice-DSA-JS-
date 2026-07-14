dd65b8236b0cd81f6c29be2727e524f573c2ed0d
lavanyamishra <100487476+lavanyamishra@users.noreply.github.com>
2026-07-14 22:29:55 +0530

    Time: 3 ms (24.94%), Space: 59.4 MB (41.42%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.js
@@ -3,12 +3,11 @@
  * @return {string}
  */
 var largestOddNumber = function (num) {
-    let n = num.length
-    for (let i = n - 1; i >= 0; i--) {
-        if (Number(num[i]) % 2 !== 0) {
-            return num.slice(0, i + 1);
+    maxOdd = "";
+    for(let i= num.length-1;i>=0;i--){
+        if(num[i]%2 === 1){
+           return num.slice(0,i+1)
         }
     }
-
-    return '';
+    return "";
 };
